c199e8c82ab70f8e6212f453564d33951cd47da0
jkenneth24 <kengerero@yahoo.com>
2023-03-08 14:58:53 +0800

    logo change

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -8,14 +8,14 @@
             <b></b>
             <a class="navbar-brand" href="javascript:void(0)">
                 <span style="">
-                    <img src="assets/images/cocsslogo22.png" alt="homepage" style="width: 130px;"/>
+                    <h3 class="text-white ms-5 mb-0" style="font-weight: 700 !important;">SorSU Clinic</h3>
                 </span>
             </a>
         </div>
 
         <div class="navbar-collapse">
             <ul class="navbar-nav mr-auto mt-md-0 ">
-                <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><img src="assets/images/cocsslogo22.png" alt="homepage" class="dark-logo " style="width: 130px; margin-left: 0px; vertical-align: text-top;"/></a> </li>
+                <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><h3 class="text-white mb-0" style="font-weight: 700 !important;">SorSU Clinic</h3></a> </li>
                 <li class="nav-item"> <a class="nav-link sidebartoggler hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="icon-arrow-left-circle"></i></a> </li>
             </ul>
             <a href="{{ route('login') }}" class="nav-item text-light me-5">Log In</a>
