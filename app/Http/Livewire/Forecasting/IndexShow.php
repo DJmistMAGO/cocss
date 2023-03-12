f04a099cbf85611adf370b224a42691ec7593861
johnkenneth24 <kengerero@yahoo.com>
2023-03-13 04:01:54 +0800

    done ui for now

---
+++ app/Http/Livewire/Forecasting/IndexShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Forecasting;
+
+use Livewire\Component;
+
+class IndexShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.forecasting.index-show');
+    }
+}
---
+++ resources/views/livewire/forecasting/index-show.blade.php
@@ -0,0 +1,30 @@
+<div class="container-fluid containerfluidneed" style="padding: 20px 20px;">
+    <div class="row">
+        <div class="col-12">
+            <div class="card" style="margin-bottom: 0px;">
+                <div class="card-body" style="padding-top: 1rem; padding-bottom: .9rem;">
+                    <div class="row page-titles" style="padding-bottom: 0px;">
+                        <div class="col-md-6 align-self-center">
+                            <h3 class="text-themecolor mb-0 mt-0" style="font-weight: 500" data-toggle="modal"
+                                data-target="#create">Forecasting</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid"
+        style="padding: 15px 15px; background-color: white; min-height: auto; margin-top: 15px;">
+        <div class="row">
+            <div class="col-12">
+                <div class="mb-3" style="overflow: auto;">
+                    Medicine To Order:
+                    <br>
+                // A comment!!!
+                <br>
+                must be a graph!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
