03fbe59c41268cf985d42d90a70161f35f907e08
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-04-25 15:24:31 +0800

    edit Forecast

--- app/Http/Livewire/Forecasting/IndexShow.php
+++ app/Http/Livewire/Forecasting/IndexShow.php
@@ -13,41 +13,14 @@ use App\Models\MedicineInventory;
 class IndexShow extends Component
 {
 
-    // will get the forecasted quantity of the medicine on the next month using rule-based method (simple moving average)
-    // public function forecasting()
-    // {
-    //     $quantities = [];
-
-    //     $inventory = MedicineInventory::orderBy('med_name', 'asc')
-    //         ->orderBy('exp_date', 'asc')
-    //         ->get();
-
-    //     foreach ($inventory as $medicine) {
-    //         $expirationDate = Carbon::parse($medicine->exp_date); // will get the expiration date of the medicine
-    //         $oneMonthFromNow = Carbon::now()->addMonth();         // will get the date of the next month
-
-    //         if ($expirationDate > $oneMonthFromNow) {             // will check if the expiration date is greater than the date of the next month
-    //             $daysUntilExpiration = $expirationDate->diffInDays($oneMonthFromNow);  // will get the difference in days between the expiration date and the date of the next month
-    //             $predictedQuantity = $medicine->med_quantity - ($daysUntilExpiration * $medicine->med_quantity / 30);  // will get the predicted quantity of the medicine on the next month
-    //             $quantities[] = [
-    //                 'name' => $medicine->med_name,
-    //                 'quantity' => max($predictedQuantity, 0),
-    //                 'expiration_date' => $expirationDate->format('Y-m-d'),
-    //             ];
-    //         }
-    //     }
-
-    //     return $quantities;  // will return the predicted quantity of the medicine on the next month
-    // }
-
-
     // will get the forecasted order quantity of the medicine on the next month using rule-based method (simple moving average)
     public function orderPrediction()
     {
         $orderQuantities = [];
 
-        $inventory = MedicineInventory::orderBy('med_name', 'asc')
-            ->orderBy('exp_date', 'asc')
+        // order by descending order of medicine name and expiration date
+        $inventory = MedicineInventory::orderBy('med_name', 'desc')
+            ->orderBy('exp_date', 'desc')
             ->get();
 
         // Calculate total predicted quantity of each medicine for the next month
@@ -56,27 +29,31 @@ class IndexShow extends Component
             $oneMonthFromNow = Carbon::now()->addMonth();  // will get the date of the next month
 
             if ($expirationDate < $oneMonthFromNow) {
-                $daysUntilExpiration = $expirationDate->diffInDays($oneMonthFromNow);
-                $predictedQuantity = $medicine->med_quantity - ($daysUntilExpiration * $medicine->med_quantity / 30);
+                $daysUntilExpiration = $expirationDate->diffInDays($oneMonthFromNow); // will get the difference of the expiration date and the date of the next month
+                $predictedQuantity = $medicine->med_quantity - ($daysUntilExpiration * $medicine->med_quantity / 30); // will get the predicted quantity of the medicine on the next month
 
+                // Initialize order quantity to 0 if it doesn't exist
                 if (!isset($orderQuantities[$medicine->med_name])) {
                     $orderQuantities[$medicine->med_name] = 0;
                 }
 
+                // Add predicted quantity to order quantity
                 $orderQuantities[$medicine->med_name] += max($predictedQuantity, 0);
             }
         }
 
         // Calculate order quantities based on minimum order quantity and current stock level
         $orders = [];
+
+        // will get the minimum order quantity and the current stock level of the medicine
         foreach ($orderQuantities as $name => $quantity) {
             $medicine = MedicineInventory::where('med_name', $name)->first();
 
             if ($medicine) {
-                $minOrderQuantity = 500; // TODO: Get minimum order quantity from database
-                $currentStockLevel = $medicine->med_quantity; // TODO: Get current stock level from database
+                $minOrderQuantity = 100;  // will get the minimum order quantity of the medicine
+                $currentStockLevel = $medicine->med_quantity;  // will get the current stock level of the medicine
 
-                $orderQuantity = max(intval($minOrderQuantity - $currentStockLevel + $quantity), 0);
+                $orderQuantity = max(intval($minOrderQuantity - $currentStockLevel + $quantity), 0);  // will get the order quantity of the medicine
                 $orders[] = [
                     'name' => $name,
                     'quantity' => $orderQuantity,
@@ -84,7 +61,7 @@ class IndexShow extends Component
             }
         }
 
-        return $orders;
+        return $orders; // will return the order quantity of the medicine
     }
 
 
@@ -106,6 +83,7 @@ class IndexShow extends Component
             );
         }
 
+        // will get the order quantity of the medicine
         $orders = $this->orderPrediction();  // will get the value from the forecasting() method
 
         return view('livewire.forecasting.index-show', compact('categories', 'data', 'medicines', 'orders'));
